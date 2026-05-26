e8833e7d7fa1610189bcacef7554ec1744dfb3c9
Lluvia A. <aguilarllluvia@gmail.com>
2026-05-26 20:33:35 +0200

    Delete record and insert multiple new entries
    Inserted multiple records into formlluviaaguilar table after deleting a specific record.

--- sql/update.sql
+++ sql/update.sql
@@ -14,3 +14,11 @@ WHERE message LIKE '%test%';
 -- EliminaR  todos los registros con email pedro@gmail.com. (ana_filtro@mail.com) ya que no tengo pedrogmail en mi base de datos
 DELETE FROM formlluviaaguilar 
 WHERE email = 'ana_filtro@mail.com';
+
+INSERT INTO formlluviaaguilar (name, age, email, subject, message) VALUES
+('Ana', 25, 'ana_filtro@mail.com', 'information', 'testmessage'),
+('Maria', 18, 'maria_filtro@mail.com', 'job', 'testmessage'),
+('Javier', 50, 'javier_filtro@mail.com', 'job', 'testmessage'),
+('Marisol', NULL, 'marisol_filtro@mail.com', 'other', 'testmessage'),
+('Sofia', 33, 'sofia_filtro@mail.com', 'information', 'testmessage'),
+(NULL, 42, 'esther_filtro@mail.com', 'job', 'testmessage');
